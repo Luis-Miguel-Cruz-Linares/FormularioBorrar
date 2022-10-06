d27b0bbfc244f807dfe4e54eeb5dda93a0610caf
Luis-Miguel-Cruz-Linares <a3521110400@alumno.uttehuacan.edu.mx>
2022-10-06 08:23:29 -0500

    04-formulario version 2

--- src/assets/components/Formulario.vue
+++ src/assets/components/Formulario.vue
@@ -1,14 +1,65 @@
+<script>
+    export default{
+        data: () => ({
+            proyecto: "",
+            tipo: "",
+            urgente: false,
+            proyectos: [],
+            numeroProyectos:0,
+        }),
+        methods:{
+            registrarProyecto(){
+                const proyecto = {
+                    proyecto: this.proyecto,
+                    tipo: this.tipo,
+                    urgente: this.urgente,
+                    completado:false
+                };
+                
+                this.proyectos.push(proyecto);
+                this.proyecto = "";
+                this.tipo = "";
+                this.urgente = false;
+            },
+            cambiarEstado(proyecto, campo) {
+               // this.proyectos[id].urgente = !this.proyectos[id].urgente;
+               //console.log(proyecto,campo);
+               proyecto[campo] = !proyecto[campo];
+            }
+        },
+        computed: {
+            numeroProyectos() {
+                return this.proyectos.length;
+            },
+        },
+    };
+</script>
 <template>
 
-    <form @submit.prevent="registrarProyecto">
-      <div class="mb-3">
-        <label  class="form-label">Proyecto</label>
-        <input v-model.trim="proyecto" type="text" class="form-control" required />
-      </div>
+    <div class="row">
+        <div class="col-12 mb-4">
+            <h3 class="text-center">Progreso: 0%</h3>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" 
+                role="progressbar" 
+                aria-valuenow="25" 
+                aria-valuemin="0"
+                aria-valuemax="100"
+                style="width: 55%;">
+                </div>
+            </div>
+        </div>
 
-      <div class="mb-3">
-        <label  class="form-label">Actividad</label>
-        <select v-model="tipo" class="form-select" required>
+        <div class="col-12 col-md-5">
+            <form @submit.prevent="registrarProyecto">
+                <div class="mb-3">
+                <label  class="form-label">Proyecto</label>
+                <input v-model.trim="proyecto" type="text" class="form-control" required />
+                </div>
+
+        <div class="mb-3">
+            <label  class="form-label">Actividad</label>
+            <select v-model="tipo" class="form-select" required>
             <option disabled selected value="">Selecciona un tipo de actividad</option>
             <option>Aplicaciones Wen con Vue.js</option>
             <option>Backend Services con Node.js</option>
@@ -24,7 +75,13 @@
 
       <button type="submit" class="btn btn-primary">Guardar</button>
     </form>
-    <hr>
+        </div>
+
+        <div class="col-12 col-md-7">
+            <h3>
+        
+        Total Proyectos: {{numeroProyectos}}
+    </h3>
     <!-- <pre>
         {{ proyecto }}
         {{ tipo }}
@@ -32,49 +89,31 @@
     </pre> -->
 
     <div class="table-responsive">
-        <table class="table table-dark">
+        <!--hover para que pinte en donde estoy posicionado-->
+        <table class="table table-dark table-hover">  
             <thead>
                 <tr>
                     <th>#</th>
                     <th>Proyecto</th>
                     <th>Tipo</th>
                     <th>Urgente</th>
+                    <th>Completado</th>
                 </tr>
             </thead>
 
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Lavarel app</td>
-                    <td>Lavarel</td>
-                    <td class="bg-success">SI | NO</td>
+                <tr v-for="(proyecto,index) in proyectos" :key="index">
+                    <td>{{index + 1}}</td>
+                    <td>{{proyecto.proyecto}}</td>
+                    <td>{{proyecto.tipo}}</td>
+                    <td @click="cambiarEstado(proyecto,'urgente')" :class="proyecto.urgente ? 'bg-success' : 'bg-danger'">{{proyecto.urgente ? "Si" : "No"}}</td>
+                    <td @click="cambiarEstado(proyecto, 'completado')" :class="proyecto.completado ? 'bg-success' : 'bg-danger'">{{proyecto.completado ? "Completo" : "Incompleto"}}</td>
                 </tr>
             </tbody>
         </table>
     </div>
+        </div>
+    </div>
 
 </template>
 
-<script>
-    export default{
-        data: () => ({
-            proyecto: "",
-            tipo: "",
-            urgente: false,
-            proyectos: [],
-        }),
-        methods:{
-            registrarProyecto(){
-                const proyecto = {
-                    proyecto: this.proyecto,
-                    tipo: this.tipo,
-                    urgente: this.urgente,
-                };
-                this.proyectos.push(proyecto);
-                this.proyecto = "";
-                this.tipo = "";
-                this.urgente = false;
-            },
-        },
-    };
-</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container mt-3">
+        <div class="row">
+          <div class="col-12 col-md-3">
+            <h1>Formularios</h1>
+          </div>
+
+          <div class="row">
+          <div class="col-12 col-md-3">
+            <img :src="url" :alt="alt" width="100">
+          </div>
+          </div>
+          
+        </div>
+      </div>
+    <hr/>
+</template>
+
+<script>
+    export default {
+      data: () => ({
+      url: "https://cdn.svgporn.com/logos/vue.svg",
+      alt: "Vue.js", 
+      nombre:"",
+      edad:0,
+      correo: "",
+    }),
+    
+    };
+    
+  </script>
